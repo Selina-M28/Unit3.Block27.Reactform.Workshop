00d2602d5ea1560e2cf8b8518adf7dc2fb032874
Selina <selina.mena@gmail.com>
2025-01-29 13:13:03 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Authenticate from "./Components/Authenticate/Authenticate";
+import SignUpForm from "./Components/SignUpForm/Signup"
+
+function App() {
+
+  return (
+    <>
+    <SignUpForm/>
+    <Authenticate/>
+      App
+    </>
+  )
+}
+
+export default App
