1edcb1d28776ee79edda037c38eb3b538469aabc
Selina <selina.mena@gmail.com>
2025-01-29 13:17:32 -0500

    test

--- src/Components/SignUpForm/Signup.jsx
+++ src/Components/SignUpForm/Signup.jsx
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function Signup() {
+  const [username,setUsername] = useState("");
+  const [password,setPassword] = useState("");
+  const [error,setError] = useState(null);
+  
   return (
     <div>Signup</div>
   )
